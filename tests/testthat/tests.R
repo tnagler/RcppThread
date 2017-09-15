558b896db9320cc0bc0df7b358db2bca2d4edece
Thomas Nagler <tnagler@users.noreply.github.com>
2017-09-16 00:43:15 +0200

    Thread class (#2)
    * add Thread class
    
    * add "unit tests"

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,5 +1,9 @@
 context("R-monitor")
-
 test_that("R-monitor works", {
     expect_output(RcppThreads:::testMonitor(), "RcppThreads says hi!")
+})
+
+context("Thread class")
+test_that("Thread class works", {
+    expect_output(RcppThreads:::testThreadClass(), "1234")
 })
