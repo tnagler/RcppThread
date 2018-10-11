02cf5467755c418c9146e75891c6f00e3073b2e3
tnagler <thomas.nagler@tum.de>
2018-10-11 12:35:18 +0200

    give Batch its own header

--- inst/include/RcppThread/RMonitor.hpp
+++ inst/include/RcppThread/RMonitor.hpp
@@ -14,6 +14,7 @@
 #include <mutex>
 #include <atomic>
 #include <stdexcept>
+#include <sstream>
 
 namespace RcppThread {
 
--- inst/include/RcppThread/ThreadPool.hpp
+++ inst/include/RcppThread/ThreadPool.hpp
@@ -8,56 +8,20 @@
 
 #include "RcppThread/RMonitor.hpp"
 #include "RcppThread/Rcout.hpp"
+#include "RcppThread/Batch.hpp"
 
 #include <vector>
 #include <thread>
 #include <queue>
 #include <condition_variable>
-#include <future>
+#include <future>      // std::shared_future
 #include <mutex>
 #include <atomic>
-#include <cstddef>
-#include <cmath>
+#include <memory>      // std::shared_ptr
+#include <atomic>
+#include <functional>  // std::function
 
 namespace RcppThread {
-
-struct Batch {
-    ptrdiff_t begin;
-    ptrdiff_t end;
-};
-
-
-inline size_t computeBatchSize(size_t nTasks, size_t nThreads)
-{
-    if (nTasks < nThreads)
-        return nTasks;
-    return nThreads * (1 + std::floor(std::log(nTasks / nThreads)));
-}
-
-
-inline std::vector<Batch> createBatches(ptrdiff_t begin,
-                                        size_t nTasks,
-                                        size_t nThreads,
-                                        size_t nBatches)
-{
-    nThreads = std::max(nThreads, static_cast<size_t>(1));
-    if (nBatches == 0)
-        nBatches = computeBatchSize(nTasks, nThreads);
-    nBatches = std::min(nTasks, nBatches);
-    std::vector<Batch> batches(nBatches);
-    size_t    minSize = nTasks / nBatches;
-    ptrdiff_t remSize = nTasks % nBatches;
-
-    for (size_t i = 0, k = 0; i < nTasks; k++) {
-        ptrdiff_t bBegin = begin + i;
-        ptrdiff_t bSize  = minSize + (remSize-- > 0);
-        batches[k] = Batch{bBegin, bBegin + bSize};
-        i += bSize;
-    }
-
-    return batches;
-}
-
 
 //! Implemenation of the thread pool pattern based on `Thread`.
 class ThreadPool {
