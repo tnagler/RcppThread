b6be9e4ad393bf106b22c165912860ba363bcbeb
tnagler <thomas.nagler@tum.de>
2018-10-30 20:42:51 +0100

    optimize batch size + fix empty batches

--- inst/include/RcppThread/Batch.hpp
+++ inst/include/RcppThread/Batch.hpp
@@ -17,11 +17,11 @@ struct Batch {
     ptrdiff_t end;
 };
 
-inline size_t computeBatchSize(size_t nTasks, size_t nThreads)
+inline size_t computeNumBatches(size_t nTasks, size_t nThreads)
 {
     if (nTasks < nThreads)
         return nTasks;
-    return nThreads * (1 + std::floor(std::log(nTasks / nThreads)));
+    return nThreads * std::floor(std::sqrt(nTasks / nThreads));
 }
 
 inline std::vector<Batch> createBatches(ptrdiff_t begin,
@@ -29,9 +29,11 @@ inline std::vector<Batch> createBatches(ptrdiff_t begin,
                                         size_t nThreads,
                                         size_t nBatches)
 {
+    if (nTasks == 0)
+        return {Batch{0, 0}};
     nThreads = std::max(nThreads, static_cast<size_t>(1));
     if (nBatches == 0)
-        nBatches = computeBatchSize(nTasks, nThreads);
+        nBatches = computeNumBatches(nTasks, nThreads);
     nBatches = std::min(nTasks, nBatches);
     std::vector<Batch> batches(nBatches);
     size_t    minSize = nTasks / nBatches;
