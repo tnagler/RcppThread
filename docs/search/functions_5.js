506dd4f857ea99e4ab50eb6960edae6ab84cdeac
tnagler <thomas.nagler@tum.de>
2018-11-05 13:34:10 +0100

    update docs

--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['parallelfor',['parallelFor',['../class_rcpp_thread_1_1_thread_pool.html#a2712f7443ce5f097207628d7aff84482',1,'RcppThread::ThreadPool::parallelFor()'],['../namespace_rcpp_thread.html#a402346abe47508d529d731c3381dc0d5',1,'RcppThread::parallelFor()']]],
+  ['parallelfor',['parallelFor',['../class_rcpp_thread_1_1_thread_pool.html#a5f6f7d3b3662bcbf61189509d06d07d0',1,'RcppThread::ThreadPool::parallelFor()'],['../namespace_rcpp_thread.html#a402346abe47508d529d731c3381dc0d5',1,'RcppThread::parallelFor()']]],
   ['parallelforeach',['parallelForEach',['../class_rcpp_thread_1_1_thread_pool.html#a44b177dbf1297cb38019eee3343b2319',1,'RcppThread::ThreadPool::parallelForEach()'],['../namespace_rcpp_thread.html#a1c246ba8e53889c0cd5c1c24e8c6cc85',1,'RcppThread::parallelForEach()']]],
-  ['push',['push',['../class_rcpp_thread_1_1_thread_pool.html#af5272d4781f49a9cb0bdb4c4b2585196',1,'RcppThread::ThreadPool']]]
+  ['push',['push',['../class_rcpp_thread_1_1_thread_pool.html#a7e29ff4aacf798ecc3374cb6c791a857',1,'RcppThread::ThreadPool']]],
+  ['pushreturn',['pushReturn',['../class_rcpp_thread_1_1_thread_pool.html#a2fc91558add3a45fa109a9721b5c8dfb',1,'RcppThread::ThreadPool']]]
 ];
