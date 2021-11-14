c8817adb0f6542cc4a211157859df8460893f0b5
tnagler <thomas.nagler@tum.de>
2021-11-14 22:10:04 +0100

    test runs :/

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -3,7 +3,7 @@ context("Compile test functions")
 Rcpp::sourceCpp(file = normalizePath("../tests.cpp"))
 
 
-runs <- 100
+runs <- 1
 for (run in seq_len(runs)) {
     context(paste0("---------------------------- run ", run, "/", runs))
     test_that("start", expect_true(TRUE))
