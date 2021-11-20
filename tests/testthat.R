f2f80fb6a6193be4317e9f9472c69bc094584c9f
tnagler <thomas.nagler@tum.de>
2021-11-20 20:56:23 +0100

    run nothing

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,11 +2,3 @@ library(testthat)
 library(RcppThread)
 
 test_check("RcppThread")
-
-my_print <- function(txt) {
-    time <- Sys.time()
-    msg <- paste0("[", time, "]  ", txt)
-    cat(msg)
-}
-
-my_print("DOOOONE")
