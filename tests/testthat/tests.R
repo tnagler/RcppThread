ac736d378b9ce3fa35d89f3d65abe2383f9bad55
tnagler <thomas.nagler@tum.de>
2021-11-21 21:43:17 +0100

    clean up prints

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,11 +1,3 @@
-my_print <- function(txt) {
-    time <- Sys.time()
-    msg <- paste0("[", time, "]  ", txt)
-    cat(msg)
-}
-
-my_print("compiling")
-
 ## -------------------------------------------------------
 context("Compile test functions")
 Rcpp::sourceCpp(file = normalizePath("../tests.cpp"))
@@ -31,48 +23,39 @@ for (run in seq_len(runs)) {
 
     ## -------------------------------------------------------
     context("Thread pool")
-    my_print("push works \n")
 
     test_that("push works", {
         testThreadPoolPush()
         expect_silent(testThreadPoolPush())
     })
 
-    my_print("pushReturn works \n")
     test_that("pushReturn works", {
         expect_silent(testThreadPoolPushReturn())
     })
-    my_print("map works \n")
 
     test_that("map works", {
         expect_silent(testThreadPoolMap())
     })
-    my_print("parallelFor works \n")
 
     test_that("parallelFor works", {
         expect_silent(testThreadPoolParallelFor())
     })
 
-    my_print("nested parallelFor works \n")
     test_that("nested parallelFor works", {
         expect_silent(testThreadPoolNestedParallelFor())
     })
-    my_print("parallelForEach works \n")
 
     test_that("parallelForEach works", {
         expect_silent(testThreadPoolParallelForEach())
     })
     
-    my_print("nested parallelForEach works \n")
     test_that("nested parallelForEach works", {
         expect_silent(testThreadPoolNestedParallelForEach())
     })
-    my_print("works single threaded \n")
 
     test_that("works single threaded", {
         expect_silent(testThreadPoolSingleThreaded())
     })
-    my_print("destructible without join \n")
 
     test_that("destructible without join", {
         expect_silent(testThreadPoolDestructWOJoin())
@@ -82,30 +65,23 @@ for (run in seq_len(runs)) {
     ## -------------------------------------------------------
     context("Parallel for functions")
 
-    my_print("parallelFor works\n")
     test_that("parallelFor works", {
         expect_silent(testParallelFor())
     })
     
-    my_print("nested parallelFor works\n")
     test_that("nested parallelFor works", {
         expect_silent(testNestedParallelFor())
     })
     
-
-    my_print("parallelForEach works\n")
     test_that("parallelForEach works", {
         expect_silent(testParallelForEach())
     })
     
-    
     test_that("nested parallelForEach works", {
         expect_silent(testNestedParallelForEach())
     })
-    my_print("nested parallelForEach works\n")
     
     # ------------------------------------------------------
-    my_print("progress tracking\n")
     context("Progress tracking")
     test_that("ProgressCounter works", {
         expect_output(testProgressCounter(), "100% \\(done\\)")
@@ -115,5 +91,3 @@ for (run in seq_len(runs)) {
         expect_output(testProgressBar(), "100% \\(done\\)")
     })
 }
-
-my_print("done testing\n")
