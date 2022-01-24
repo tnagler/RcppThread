0d9f729b26aae1305d83299f60462bb7e3b7a7b1
tnagler <mail@tnagler.com>
2022-01-24 23:37:02 +0100

    safe exception tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -408,7 +408,7 @@ testThreadPoolExceptionHandling()
     std::exception_ptr eptr = nullptr;
     try {
         pool.push([] { throw std::runtime_error("test error"); });
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
         for (size_t i = 0; i < 10; i++) {
             pool.push([&] {});
         }
@@ -424,6 +424,7 @@ testThreadPoolExceptionHandling()
 
     // poool should be functional again
     pool.push([] { throw std::runtime_error("test error"); });
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
     try {
         pool.wait();
     } catch (...) {
