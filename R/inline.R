333a994ccbc3153e8568af77040dd4481668a17a
tnagler <mail@tnagler.com>
2022-01-27 11:23:28 +0100

    libstdc++

--- R/inline.R
+++ R/inline.R
@@ -1,10 +1,11 @@
 inlineCxxPlugin <- function(...) {
     plugin <- Rcpp::Rcpp.plugin.maker(
-        include.before = "#include <RcppThread.h>",
-        libs           = "-latomic",
-        package        = "RcppThread"
+        include.before  = "#include <RcppThread.h>",
+        package        = "RcppThread",
+        Makevars = "CXX += -stdlib=libstdc++"
     )
     settings <- plugin()
     settings$env$USE_CXX11 <- "yes"
     settings
 }
+
