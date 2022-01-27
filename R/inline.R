2e27e9190bafbe3288161b4cc62889f33c7527af
tnagler <mail@tnagler.com>
2022-01-27 11:24:01 +0100

    CXX_FLAGS

--- R/inline.R
+++ R/inline.R
@@ -2,7 +2,7 @@ inlineCxxPlugin <- function(...) {
     plugin <- Rcpp::Rcpp.plugin.maker(
         include.before  = "#include <RcppThread.h>",
         package        = "RcppThread",
-        Makevars = "CXX += -stdlib=libstdc++"
+        Makevars = "CXX_FLAGS = -stdlib=libstdc++"
     )
     settings <- plugin()
     settings$env$USE_CXX11 <- "yes"
