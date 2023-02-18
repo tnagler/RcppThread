63084c9b39d2bebc04a72306749a3b75d8822658
tnagler <thomas.nagler@tum.de>
2023-02-18 13:06:22 +0100

    fix flags for sourceCpp

--- R/inline.R
+++ R/inline.R
@@ -2,7 +2,7 @@ inlineCxxPlugin <- function(...) {
     settings <- Rcpp::Rcpp.plugin.maker(
         include.before  = "#include <RcppThread.h>",
         package = "RcppThread",
-        libs = if (Sys.info()["sysname"] == "Linux") "-latomic -lpthread" else ""
+        libs = RcppThread::LdFlags()
     )()
     settings$env$USE_CXX11 <- "yes"
     settings
